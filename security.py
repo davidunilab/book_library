f062c8fa09aa55115e9c9ec8b94958e3ec5e753f
David <davit.chincharashvili.1@iliauni.edu.ge>
2021-02-23 21:33:14 +0400

    removed encode

--- security.py
+++ security.py
@@ -3,7 +3,7 @@ from models.users import User
 
 def authenticate(username, password):
     user = User.find_by_username(username)
-    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+    if user and safe_str_cmp(user.password, password):
         return user
 
 
